2d70d65a2039fc8591bc2bf7c3ddf9a167eae59f
shiaochiao <shiaochiao@outlook.com>
2025-02-28 12:53:42 -0800

    Add files via upload

--- Scripts/Google/searchdomaincheck.js
+++
@@ -1,9 +0,0 @@
-if($response.status != 200){
-	$done({});
-}else{
-	let jbody = JSON.parse($response.body);
-	jbody["domain"] = ".google.com";
-	jbody["searchAuthority"] = "https://www.google.com";
-	jbody=JSON.stringify(jbody);
-	$done({body:jbody});
-}
